aba3f992cb6bba44e812e66054720ac4702ff64b
Karunlal <karunlalp@gmail.com>
2024-02-01 22:05:32 +0530

    added all

---
+++ src/components/Player.jsx
@@ -0,0 +1,23 @@
+import { useState, useRef } from 'react'
+
+export default function Player() {
+  const playerName = useRef()
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null)
+  // const [submitted, setSubmitted] = useState(false)
+
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value)
+    // setSubmitted(true)
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <p>
+        <input ref={playerName} type="text" />
+
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  )
+}
